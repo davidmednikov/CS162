6dad4ae20f36831b9bb29d76a3edc6181a6fea30
Jose Cisneros <cisnejos@oregonstate.edu>
2017-02-20 16:59:40 -0700

    2nd commit

--- GroupProject/main.cpp
+++ GroupProject/main.cpp
@@ -4,3 +4,10 @@
  ** Date: 2/26/2017
  ** Description: a Rock, Paper, Scissors game simulation.
  *********************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+}
