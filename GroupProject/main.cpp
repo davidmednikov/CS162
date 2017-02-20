6f15a31a8cb1d7254f5cfa6dde9dc19e44b98740
Jose Cisneros <cisnejos@oregonstate.edu>
2017-02-20 16:57:33 -0700

    1st commit

---
+++ GroupProject/main.cpp
@@ -0,0 +1,6 @@
+/*********************************************************************
+ ** Program name:  Play Rock, Paper, Scissors!!!
+ ** Author: Jose Cisneros, 
+ ** Date: 2/26/2017
+ ** Description: a Rock, Paper, Scissors game simulation.
+ *********************************************************************/
