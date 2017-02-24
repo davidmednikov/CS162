a9ac6c1f604237a87f13589d1e65517134b6d8d0
csgaroutte <chrisgaroutte@gmail.com>
2017-02-23 19:56:51 -0800

    added overloaded = operator

--- GroupProject/RPSGame.cpp
+++ GroupProject/RPSGame.cpp
@@ -36,4 +36,25 @@ void RPSGame::endGame(){}
  *********************************************************************/
 void RPSGame::round(){}
 
+/*********************************************************************
+ ** Description: Overloaded Assignment (=) Operator
+ ** Parameter: Reference to object of type RPSGame
+ ** Returns: Reference to object of type RPSGame (*this)
+ *********************************************************************/
+RPSGame& operator=(const RPSGame& game)
+{
+	if(this == &game)
+	{
+		return *this;
+	}
+	
+	this->human = game.human;
+	this->computer = game.computer;
+	this->human_wins = game.human_wins;
+	this->computer_wins = game.computer_wins;
+	this->ties = game.ties;
+	
+	return *this;
+}
+
 	
